4aa8fbedb528bafa9873a951750b5d36e38927d8
AseefBandey <aseefahmad46@gmail.com>
2025-09-07 20:15:25 +0530

    Update The File

--- PyLearn.py
+++ PyLearn.py
@@ -781,6 +781,8 @@
        
 #         for i in range(0,len(nums1)):
 #             for j in range(0,len(nums2)):
-                
+#                 if nums1[i] == nums2[j]:
+                    
+#                     intersection.append(nums1[i]) 
 
    
