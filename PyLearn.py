6a9a08f4f7313cdf71b1704108c63cc67c40c8d2
AseefBandey <aseefahmad46@gmail.com>
2025-09-07 20:04:20 +0530

    Update The File

--- PyLearn.py
+++ PyLearn.py
@@ -771,4 +771,5 @@
 # print(solution.singleNumber(nums))  
 #------------------------------------------------------------------------------------------#
 #                                  Intersection of Two Arrays II
-  
+#Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+# Example 1:
