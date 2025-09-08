527dfecbdaf8463f29295b7ef6838d6d318981f3
AseefBandey <aseefahmad46@gmail.com>
2025-09-09 00:23:43 +0530

    Update The File

--- PyLearn.py
+++ PyLearn.py
@@ -824,7 +824,8 @@
 #------------------------------------------------------------------------------------------#
 #                                             Move Zeroes
 
-# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative
+#  order of the non-zero elements.
 
 # Note that you must do this in-place without making a copy of the array.
 
@@ -834,6 +835,24 @@
 
 # Input: nums = [0,1,0,3,12]
 # Output: [1,3,12,0,0]
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        j = 0  # index for next non-zero
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[j] = nums[i]
+                j += 1
+
+        # fill remaining with zeros
+        for k in range(j, len(nums)):
+            nums[k] = 0
+
+        return nums
+
+nums = [0,0,1]
+solution = Solution()
+print(solution.moveZeroes(nums))  # [1,3,12,0,0]
 
 
 
