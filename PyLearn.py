14ff22387d47ad878f9978d7177b7370b677ecb8
AseefBandey <aseefahmad46@gmail.com>
2025-09-11 22:19:37 +0530

    Update The File

--- PyLearn.py
+++ PyLearn.py
@@ -853,6 +853,33 @@
 # nums = [0,0,1]
 # solution = Solution()
 # print(solution.moveZeroes(nums))  # [1,3,12,0,0] 
+#------------------------------------------------------------------------------------------#
+#                                      Maximum Subarray
+
+#Given an integer array nums, find the  with the largest sum, and return its sum.
+
+
+# Example 1:
+
+# Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+# Output: 6
+# Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+# Example 2:
+
+# Input: nums = [1]
+# Output: 1
+# Explanation: The subarray [1] has the largest sum 1.
+                                   
+# class Solution:
+#     def maxSubArray(self, nums):
+#         maxSub, curSum = nums[0], 0
+#         for num in nums:
+#             if curSum < 0:
+#                 curSum = 0
+#             curSum += num
+#             maxSub = max(maxSub, curSum)
+#         return maxSub
 
 
 
