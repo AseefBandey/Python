735d2c916955ed0e301b361274808a849c738215
AseefBandey <aseefahmad46@gmail.com>
2025-09-17 20:12:27 +0530

    Update The File

--- PyLearn.py
+++ PyLearn.py
@@ -962,10 +962,12 @@
 # 1 2
 # 1
 
-n = input("Give N: ")
-n = int(n)
+# n = input("Give N: ")
+# n = int(n)
+# for i in range(n,0,-1):
+#     for j in range(1, i+1):  # Change: j goes from 1 to i+1
+#         print(j, end=" ")     # Added space for better formatting
+#     print()
 
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        print(j, end="")
-    print()
+
+
