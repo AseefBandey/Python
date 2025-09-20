8087aaf3bd663fba47459726d292f0b699d8c19d
AseefBandey <aseefahmad46@gmail.com>
2025-09-20 13:27:36 +0530

    Update The File

--- PyLearn.py
+++ PyLearn.py
@@ -1171,10 +1171,10 @@
 # n =len(nums)
 # for i in range(n-2,-1,-1):
 #     for j in range(0,i+1):
-        if nums[j]>nums[j+1]:
-            nums[j],nums[j+1] = nums[j+1],nums[j]
+#         if nums[j]>nums[j+1]:
+#             nums[j],nums[j+1] = nums[j+1],nums[j]
 
-print(nums)
+# print(nums)
         
             
 
