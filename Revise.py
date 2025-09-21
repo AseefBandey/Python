5569e89b2c4f2c48c56662e1929e55cc27abd963
AseefBandey <aseefahmad46@gmail.com>
2025-09-21 12:59:03 +0530

    Update The File

---
+++ Revise.py
@@ -0,0 +1,45 @@
+# Input Format: N = 3
+# Result: 
+# 1
+# 1 2 
+# 1 2 3
+
+# n = int(input("Enter N: "))
+
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j ,  end="")
+#     print()
+
+# Input Format: N = 3
+# Result: 
+# 1 2 3
+# 1 2
+# 1
+
+# n = int(input("Enter N: "))
+# for i in range(n,0,-1):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+
+
+# Input Format: N = 3
+# Result: 
+#   *  
+#  *** 
+# ***** 
+
+
+# n = int(input("Enter N: "))
+
+
+# for i in range (1, n+1):
+
+#     spaces = n - i
+#     print(" " * spaces,end="")
+
+
+#     stars = 2 * i - 1
+#     print("*" * stars)
+
