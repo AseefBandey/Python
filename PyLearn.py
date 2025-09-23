5bde641a49ab524a848634faf755445484bd15eb
AseefBandey <aseefahmad46@gmail.com>
2025-09-23 11:57:03 +0530

    Update The File

--- PyLearn.py
+++ PyLearn.py
@@ -1206,36 +1206,36 @@
 # result = insertion_sort(nums)
 # print("Sorted:", result)
 
-def mergeSort(arr):
-  if len(arr) <= 1:
-    return arr
+# def mergeSort(arr):
+#   if len(arr) <= 1:
+#     return arr
 
-  mid = len(arr) // 2
-  leftHalf = arr[:mid]
-  rightHalf = arr[mid:]
+#   mid = len(arr) // 2
+#   leftHalf = arr[:mid]
+#   rightHalf = arr[mid:]
 
-  sortedLeft = mergeSort(leftHalf)
-  sortedRight = mergeSort(rightHalf)
+#   sortedLeft = mergeSort(leftHalf)
+#   sortedRight = mergeSort(rightHalf)
 
-  return merge(sortedLeft, sortedRight)
+#   return merge(sortedLeft, sortedRight)
 
-def merge(left, right):
-  result = []
-  i = j = 0
+# def merge(left, right):
+#   result = []
+#   i = j = 0
 
-  while i < len(left) and j < len(right):
-    if left[i] < right[j]:
-      result.append(left[i])
-      i += 1
-    else:
-      result.append(right[j])
-      j += 1
+#   while i < len(left) and j < len(right):
+#     if left[i] < right[j]:
+#       result.append(left[i])
+#       i += 1
+#     else:
+#       result.append(right[j])
+#       j += 1
 
-  result.extend(left[i:])
-  result.extend(right[j:])
+#   result.extend(left[i:])
+#   result.extend(right[j:])
 
-  return result
+#   return result
 
-mylist = [3, 7, 6, -10, 15, 23.5, 55, -13]
-mysortedlist = mergeSort(mylist)
-print("Sorted array:", mysortedlist) 
+# mylist = [3, 7, 6, -10, 15, 23.5, 55, -13]
+# mysortedlist = mergeSort(mylist)
+# print("Sorted array:", mysortedlist) 
