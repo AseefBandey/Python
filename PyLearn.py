e4a3107f3c08d4309f4573c78ee2e58212307a25
AseefBandey <aseefahmad46@gmail.com>
2025-10-03 12:41:45 +0530

    Update The File

--- PyLearn.py
+++ PyLearn.py
@@ -1428,19 +1428,19 @@
 #         return nums[-1]      
         
 
-def longestSubarrayWithSumK(arr, k):
-    n = len(arr)
-    max_length = 0
+# def longestSubarrayWithSumK(arr, k):
+#     n = len(arr)
+#     max_length = 0
     
-    for i in range(n):
-        current_sum = 0
-        for j in range(i, n):
-            current_sum += arr[j]  # Add current element to running sum
+#     for i in range(n):
+#         current_sum = 0
+#         for j in range(i, n):
+#             current_sum += arr[j]  # Add current element to running sum
             
-            if current_sum == k:
-                max_length = max(max_length, j - i + 1)
+#             if current_sum == k:
+#                 max_length = max(max_length, j - i + 1)
     
-    return max_length
+#     return max_length
 
 
         
