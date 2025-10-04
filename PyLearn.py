11c2b77d4b4eea288ab166c07e89f145838e11cc
AseefBandey <aseefahmad46@gmail.com>
2025-10-04 11:59:33 +0530

    Update The File

--- PyLearn.py
+++ PyLearn.py
@@ -1443,4 +1443,14 @@
 #     return max_length
 
 
-        
+# 2sum:- 
+ 
+
+# class Solution:
+#     def twoSum(self, nums, target):
+#         n = len(nums)
+#         for i in range(n):
+#             for j in range(i + 1, n):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]  
+#         return []  
