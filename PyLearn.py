a82fa1e293457abe4fa1be31ff08575280008289
AseefBandey <aseefahmad46@gmail.com>
2025-10-08 19:57:11 +0530

    Update The File

--- PyLearn.py
+++ PyLearn.py
@@ -1637,3 +1637,18 @@
 #             for i in range(m):
 #                 matrix[i][0] = 0
 
+# row addition one row at a time
+matrix = [
+ [1, 2, 3],
+ [4, 5, 6],
+ [7, 8, 9]
+]
+sum= 0
+for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
+        sum += matrix[i][j]
+    print (sum)
+    sum = 0
+
+# coloum adddition one at a time 
+
