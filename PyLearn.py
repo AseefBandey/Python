15066dabdd1d0fd39bf1ccdbefac6ff4e5d912a5
AseefBandey <aseefahmad46@gmail.com>
2025-10-09 20:37:17 +0530

    Update The File

--- PyLearn.py
+++ PyLearn.py
@@ -1710,26 +1710,26 @@
 #                     matrix[i][j] = 0
 
 
-matrix =[[5,1,9,11],
-         [2,4,8,10],
-         [13,3,6,7],
-         [15,14,12,16]]
+# matrix =[[5,1,9,11],
+#          [2,4,8,10],
+#          [13,3,6,7],
+#          [15,14,12,16]]
 
-n,m = len(matrix),len(matrix[0])
+# n,m = len(matrix),len(matrix[0])
 
-for i in range(n):
-    for j in range(i+1, m):  # upper triangle only
-        matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+# for i in range(n):
+#     for j in range(i+1, m):  # upper triangle only
+#         matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-for i in range(n):
-    for j in range(m):
-        if j == 0:
-            matrix[i][j],matrix[i][-1] = matrix[i][-1] , matrix[i][j]
-    print(matrix)
-
-
+# for i in range(n):
+#     for j in range(m):
+#         if j == 0:
+#             matrix[i][j],matrix[i][-1] = matrix[i][-1] , matrix[i][j]
+#     print(matrix)
 
 
 
-print(matrix)
+
+
+# print(matrix)
 
