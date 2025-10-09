dab8f0cafbff24a2c54b79994feff588dabc68ea
AseefBandey <aseefahmad46@gmail.com>
2025-10-09 20:44:32 +0530

    Update The File

--- PyLearn.py
+++ PyLearn.py
@@ -1721,11 +1721,8 @@
 #     for j in range(i+1, m):  # upper triangle only
 #         matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-# for i in range(n):
-#     for j in range(m):
-#         if j == 0:
-#             matrix[i][j],matrix[i][-1] = matrix[i][-1] , matrix[i][j]
-#     print(matrix)
+# for row in matrix:
+#             row.reverse()
 
 
 
