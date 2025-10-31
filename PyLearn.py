e7e599483d2ac9718c737f15b7e7c9a0d34df4e2
AseefBandey <aseefahmad46@gmail.com>
2025-10-31 12:00:19 +0530

    v

--- PyLearn.py
+++ PyLearn.py
@@ -1781,3 +1781,19 @@
 #             i += 2           
         
 #         return nums[-1]
+
+
+class Solution(object):
+    def majorityElement(self, nums):
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else -1             
+
+        return candidate
+
+
+
