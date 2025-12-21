554fb73c612e7c03959dd9563266b66e50627f8a
AseefBandey <aseefahmad46@gmail.com>
2025-12-21 19:25:10 +0530

    Update

--- PyLearn.py
+++ PyLearn.py
@@ -1873,4 +1873,37 @@
 #             i = j + length
 
 #         return res
+
+
+
+nums = [1,2,3,4]
+
+prod = 1
+
+res =[]
+
+res = [1] * len(nums)
+for i in range(len(nums)):
+   prod = 1
+   for j in range(len(nums)):
+      if j == i : continue
+      prod = prod * nums[j]
     
+   res[i] = prod
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+        
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        
+        return res
