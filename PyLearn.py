286344c7756929eb5a437c548cca4b4463bb5337
AseefBandey <aseefahmad46@gmail.com>
2025-12-26 12:38:09 +0530

    Update

--- PyLearn.py
+++ PyLearn.py
@@ -1951,3 +1951,33 @@
 #         longest = max(longest, current_length)
 
 # return longest
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        water=0
+        left,right=0,len(height)-1
+
+        lmax,rmax=0,0
+
+        while left<right:
+            
+            if height[left]<height[right]:
+                if height[left]>=lmax:
+                    lmax=height[left]
+                else:
+                    water+=lmax-height[left]
+                left+=1
+
+            else:
+                if height[right]>=rmax:
+                    rmax=height[right]
+                
+                else:
+                    water+=rmax-height[right]
+                right-=1
+                
+
+        return water
